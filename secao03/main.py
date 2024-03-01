27262d20eeb841fef64eb9bfd823bd684af45693
kcarww <carlos.oe.cc@gmail.com>
2024-03-01 19:13:31 -0300

    feat: http GET, POST, DELETE, PUT

---
+++ secao03/main.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+from models import Curso
+from typing import Optional, List
+from fastapi.responses import JSONResponse, Response
+app = FastAPI()
+
+cursos = {
+    1: {
+        "titulo": "Java",
+        "aulas": 112,
+        "horas": 58
+    },
+    2: {
+        "titulo": "Javascript",
+        "aulas": 100,
+        "horas": 50
+    }
+}
+
+@app.get('/cursos')
+async def getCursos():
+    return cursos
+
+@app.get('/cursos/{curso_id}')
+async def getCursoById(curso_id: int):
+    try:
+        return cursos[curso_id]
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def createCurso(curso: Curso):
+    if curso.id not in cursos:
+        cursos[curso.id] = curso
+        return cursos[curso.id]
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Curso já existe")
+
+
+@app.put('/cursos/{curso_id}', status_code=status.HTTP_202_ACCEPTED)
+async def updateCurso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        return cursos[curso_id]
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
+@app.delete('/cursos/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def deleteCurso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,  reload=True)
