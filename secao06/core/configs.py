2b912b1d12e48ce3b099278378f5918f0787b026
kcarww <carlos.oe.cc@gmail.com>
2024-09-04 01:08:20 -0300

    feat: created tables

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -1,13 +1,13 @@
 from typing import List
-from pydantic import BaseSettings
-from sqlalchemy.axt.declarative import declarative_base
-
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Type
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     DB_URL: str = "mysql+asyncmy://root:@localhost:3306/faculdade"
-    DBBaseModel = declarative_base()
+    DBBaseModel: Type = declarative_base()
     
-    JWT_SECRET: srt = 'w311PgVcqiBIXEGXa_IuPpRUIdIdlx8CXU0ImmWHB1c'
+    JWT_SECRET: str = 'w311PgVcqiBIXEGXa_IuPpRUIdIdlx8CXU0ImmWHB1c'
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     
